5f0fbc7342ba21cc1df6dd24d3491e62717fa80e
RichyHBM <gitemail@richyhbm.co.uk>
2025-04-24 18:37:26 +0000

    Move server implementation in to own file

---
+++ cmd/rje/job_service_server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"errors"
+
+	"github.com/richyhbm/teleport-challenge/pkg/rje"
+	"github.com/richyhbm/teleport-challenge/proto"
+	"google.golang.org/grpc"
+)
+
+type jobServiceServer struct {
+	proto.UnimplementedJobsServiceServer
+	remoteJobRunner rje.RemoteJobRunner
+}
+
+func (jSS *jobServiceServer) Start(ctx context.Context, req *proto.JobStartRequest) (*proto.JobStartResponse, error) {
+	if req == nil {
+		return nil, errors.New("empty request")
+	}
+
+	return jSS.UnimplementedJobsServiceServer.Start(ctx, req)
+}
+
+func (jSS *jobServiceServer) Stop(ctx context.Context, req *proto.JobIdRequest) (*proto.JobStopResponse, error) {
+	if req == nil {
+		return nil, errors.New("empty request")
+	}
+
+	return jSS.UnimplementedJobsServiceServer.Stop(ctx, req)
+}
+
+func (jSS *jobServiceServer) Status(ctx context.Context, req *proto.JobIdRequest) (*proto.JobStatusResponse, error) {
+	if req == nil {
+		return nil, errors.New("empty request")
+	}
+
+	return jSS.UnimplementedJobsServiceServer.Status(ctx, req)
+}
+
+func (jSS *jobServiceServer) Tail(req *proto.JobIdRequest, stream grpc.ServerStreamingServer[proto.JobOutputResponse]) error {
+	if req == nil {
+		return errors.New("empty request")
+	}
+
+	return jSS.UnimplementedJobsServiceServer.Tail(req, stream)
+}
